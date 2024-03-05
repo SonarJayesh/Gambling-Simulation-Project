d4f6a2156e18b2e2ecd325b500aab5639d9c677c
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-05 12:01:11 +0530

    [ADDED]: JAYESH Gambling Simulation Project Setup

---
+++ src/com/jayesh/gamblingproject/GamblingSimulationProject.java
@@ -0,0 +1,11 @@
+package com.jayesh.gamblingproject;
+
+
+public class GamblingSimulationProject {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation problem Developed by Jayesh Sonar.");
+	
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GamblingSimulationProject {
+}
