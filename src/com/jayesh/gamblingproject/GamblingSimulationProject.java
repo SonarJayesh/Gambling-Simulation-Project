d44bc1e54b56dbcaa9eee297ad8d7a18b694a93c
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-05 12:33:37 +0530

    [MODIFIED]: UC-1 is completed Setting up Stack and Bet

--- src/com/jayesh/gamblingproject/GamblingSimulationProject.java
+++ src/com/jayesh/gamblingproject/GamblingSimulationProject.java
@@ -3,9 +3,19 @@ package com.jayesh.gamblingproject;
 
 public class GamblingSimulationProject {
 	
+	private static final int STACK_PER_DAY = 100;
+	private static final int BET_PER_GAME = 1;
+	
+	public static void gameStatus()
+	{
+		System.out.println("Stack Available ="+ STACK_PER_DAY);
+		System.out.println("Bet per Day ="+ BET_PER_GAME);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation problem Developed by Jayesh Sonar.");
 	
+		gameStatus();
 	}
 
 }
