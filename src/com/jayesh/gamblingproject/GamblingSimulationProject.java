deeab73f852c59de9991ceeb8056f3c201ca7eb3
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-09 12:40:52 +0530

    [ADDED]: JAYESH UC-6 is Completed Find The Luckiest Day And UnLuckiest Day

--- src/com/jayesh/gamblingproject/GamblingSimulationProject.java
+++ src/com/jayesh/gamblingproject/GamblingSimulationProject.java
@@ -10,11 +10,19 @@ public class GamblingSimulationProject {
 	private static final int LOOSE = 0;
 	
 	private static final int[] winAmount = new int[20];
-	private static final int[] looseAmount =new int[20];
+	private static final int[] looseAmount = new int[20];
+	
+	private static final int[] won_Maximum = new int[20];
+	private static final int[] lost_Maximum = new int[20];
 	
 	private static int total_Win_Day = 0;
 	private static int total_Loose_Day = 0;
 	static int playStatus;
+	
+	static int maxWon = 0;
+	static int maxLoose = 0;
+	static int winIndex = 0;
+	static int looseIndex = 0;
 	
 	public static void gameStatus() {
 		
@@ -24,6 +32,8 @@ public class GamblingSimulationProject {
 	}
 	
 	public static void gamePlay() {
+		
+		
 		gameStatus();
 		for(int i=0; i<20; i++) {
 				
@@ -31,51 +41,74 @@ public class GamblingSimulationProject {
 			 int winStack = 0;
 			 int looseStack = 0;
 			 
-			 while(true) {
+			 	while(true) {
 
 				
-				Random random = new Random();
-				playStatus = random.nextInt(9) % 2;
+			 		Random random = new Random();
+			 		playStatus = random.nextInt(9) % 2;
 				
 		
-				if(playStatus == LOOSE) {
+			 		if(playStatus == LOOSE) {
 				
-					looseStack += 1;
-				}
-				else {
+			 			looseStack += 1;
+			 		}
+			 		else {
 				
-					winStack += 1;
-				
-				}
-				if(winStack == STACK_PER_DAY / 2) {
-					total_Win_Day += 1;
-					break;
+			 			winStack += 1;
 					
-				}
-				if(looseStack == STACK_PER_DAY /2){
-					total_Loose_Day += 1;
-					break;
-					
+			 		}
+			 		if(winStack == STACK_PER_DAY / 2) {
+			 			total_Win_Day += 1;
+			 			
+			 			if(looseStack < winStack) {
+			 				
+			 					won_Maximum[i] = winStack - looseStack;
+			 					if(maxWon < won_Maximum[i]) {
+			 						maxWon = won_Maximum[i];
+			 						winIndex = i+1;
+			 						
+			 					}
+			 			}
+			 			
+			 			break;
+			 		}
+			 		if(looseStack == STACK_PER_DAY /2) {
+			 			total_Loose_Day += 1;
+			 				if(maxLoose < looseStack) {
+			 					
+			 					lost_Maximum[i] = looseStack - winStack;
+			 					if(maxLoose < lost_Maximum[i]) {
+			 						maxLoose = lost_Maximum[i];
+			 						looseIndex = i+1;
+			 						
+			 					}
+			 				}
+			 			break;			
 				}
 
 			}
 			 
-			 winAmount[i] = winStack;
-			 looseAmount[i] = looseStack;
+			 	winAmount[i] = winStack;
+			 	looseAmount[i] = looseStack;
 			 
-			System.out.println("Winning Stack = "+ winStack);
-			System.out.println("Loose Stack = "+ looseStack);
-			System.out.println("----------------------------------");
-		}
-		System.out.println("Total Win Day Month = "+ total_Win_Day);
-		System.out.println("Total Loose Day Month = "+ total_Loose_Day);
+			 	System.out.println("Winning Stack = "+ winStack);
+			 	System.out.println("Loose Stack = "+ looseStack);
+			 	System.out.println("----------------------------------");
+			 	
+			}
+		
+			System.out.println("Total Win Day Month = "+ total_Win_Day);
+			System.out.println(winIndex + " is the Luckiest Day, Won Rs : "+ maxWon);
+			System.out.println("Total Loose Day Month = "+ total_Loose_Day);
+			System.out.println(looseIndex + " is the UnLuckiest Day, Lost Rs : "+ maxLoose);
+			
 	}		
 			
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Gambling Simulation problem Developed by Jayesh Sonar.");
+			System.out.println("Welcome to Gambling Simulation problem Developed by Jayesh Sonar.");
 		
 			gamePlay();
 			
-	}
+		}
 }
